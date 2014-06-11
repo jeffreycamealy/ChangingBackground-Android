a63da1040eb919eb7480f96666f68e5cf6e07529
raf <dslounge@gmail.com>
2014-06-11 01:14:22 -0400

    Uncommitted progress on floating activity test.

--- app/src/main/java/com/orainteractive/changingbackground/MainActivity.java
+++ app/src/main/java/com/orainteractive/changingbackground/MainActivity.java
@@ -1,15 +1,39 @@
 package com.orainteractive.changingbackground;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * The main Activity of the app
  */
 public class MainActivity extends Activity {
+
+    private Button nextBt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_blank);
+        //fire up the activity
+
+        final Intent intent = new Intent(this, FirstActivity.class);
+        startActivity(intent);
+//
+//        nextBt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(intent);
+//                overridePendingTransition(R.anim.rl_in, R.anim.rl_out);
+//                //TransitionDrawable d = (TransitionDrawable) getWindow().getDecorView().getBackground();
+//                //d.startTransition(2000);
+//            }
+//        });
+
+
     }
+
+
 }
